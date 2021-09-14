38568b45b9283f55bae1b068fdd185e21db9cb8a
Stefan Niedermann <info@niedermann.it>
2021-09-14 08:51:21 +0200

    Refactoring and persistent dark mode

--- main.js
+++ main.js
@@ -1,37 +1,64 @@
 'use strict';
 
-const darkModeClass = 'dark';
+document.addEventListener("DOMContentLoaded", async () => {
+    const resp = await fetch('./data.json');
+    const data = await resp.json();
+
+    const mainEl = document.querySelector('main');
+    const filterEl = document.querySelector('input');
+    initFilter(mainEl, filterEl, data);
+
+    const darkModeToggleEl = document.querySelector('select');
+    initDarkMode(darkModeToggleEl);
+});
+
+const initFilter = (containerEl, filterEl, data) => {
+    filterEl.addEventListener('keyup', () => render(containerEl, filter(filterEl.value, data)));
+    render(containerEl, filter(filterEl.value, data));
+}
+
 const categoryColors = ['#b6469d', '#bf678b', '#c98879', '#ddcb55', '#a5b872', '#6ea68f', '#3794ac', '#0082c9', '#2d73be', '#5b64b3', '#8855a8'];
 
 const render = (container, data) => {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
+    removeAllChildren(container);
 
     const documentFragment = document.createDocumentFragment();
     data.forEach((category, index) => {
         const categorySection = document.createElement('div');
         categorySection.style.backgroundColor = categoryColors[(index % categoryColors.length) * 2];
+        
         const categoryHeader = document.createElement('h2');
         categoryHeader.innerText = category.title;
-        categorySection.appendChild(categoryHeader);
+
         const romList = document.createElement('ul');
-        category.roms.forEach(rom => {
-            const romItem = document.createElement('li');
-            romItem.innerText = rom.name;
-            rom.links.forEach(link => {
-                romItem.appendChild(document.createElement('br'));
-                const anchor = document.createElement('a');
-                anchor.innerText = link.text;
-                anchor.setAttribute('href', link.url);
-                romItem.appendChild(anchor);
-            });
-            romList.appendChild(romItem);
-        })
+        category.roms.forEach(rom => romList.appendChild(renderRom(rom)))
+
+        categorySection.appendChild(categoryHeader);
         categorySection.appendChild(romList);
         documentFragment.appendChild(categorySection);
     });
     container.appendChild(documentFragment)
+}
+
+const removeAllChildren = (el) => {
+    while (el.firstChild) {
+        el.removeChild(el.firstChild);
+    }
+}
+
+const renderRom = (rom) => {
+    const romItem = document.createElement('li');
+    romItem.innerText = rom.name;
+    rom.links.forEach(link => renderLink(romItem, link));
+    return romItem;
+}
+
+const renderLink = (parentEl, link) => {
+    parentEl.appendChild(document.createElement('br'));
+    const anchor = document.createElement('a');
+    anchor.innerText = link.text;
+    anchor.setAttribute('href', link.url);
+    parentEl.appendChild(anchor);
 }
 
 const filter = (term, data) => {
@@ -44,23 +71,65 @@ const filter = (term, data) => {
     });
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-    const resp = await fetch('./data.json');
-    const roms = await resp.json();
-
-    const mainEl = document.querySelector('main');
-    const filterEl = document.querySelector('input');
-    const darkModeToggleEl = document.querySelector('button');
-
-    filterEl.addEventListener('keyup', () => render(mainEl, filter(filterEl.value, roms)));
-    render(mainEl, filter(filterEl.value, roms));
-
-    darkModeToggleEl.addEventListener('click', () => document.body.classList.toggle(darkModeClass));
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+const initDarkMode = (darkModeToggleEl) => {
+    const themePersistenceKey = 'theme';
+    const themePersistenceValueDark = 'dark';
+    const darkModeCSSClass = 'dark';
+    const darkModeMedia = window.matchMedia('(prefers-color-scheme: dark)');
+    const darkModeMediaListener = (e) => {
         if (e.matches) {
-            document.body.classList.add(darkModeClass);
+            document.body.classList.add(darkModeCSSClass);
         } else {
-            document.body.classList.remove(darkModeClass);
+            document.body.classList.remove(darkModeCSSClass);
+        }
+    };
+
+    switch (localStorage.getItem(themePersistenceKey)) {
+        case themePersistenceValueDark: {
+            document.body.classList.add(darkModeCSSClass);
+            darkModeToggleEl.value = 'dark';
+            break;
+        }
+        case null: {
+            darkModeMedia.addEventListener('change', darkModeMediaListener);
+            if (darkModeMedia.matches) {
+                document.body.classList.add(darkModeCSSClass);
+            } else {
+                document.body.classList.remove(darkModeCSSClass);
+            }
+            darkModeToggleEl.value = 'system';
+            break;
+        }
+        default: {
+            darkModeToggleEl.value = 'light';
+            break;
+        }
+    }
+
+    darkModeToggleEl.addEventListener('change', (e) => {
+        switch (e.target.value) {
+            case 'light': {
+                darkModeMedia.removeEventListener('change', darkModeMediaListener);
+                document.body.classList.remove(darkModeCSSClass);
+                localStorage.setItem(themePersistenceKey, 'light');
+                break;
+            }
+            case 'dark': {
+                darkModeMedia.removeEventListener('change', darkModeMediaListener);
+                document.body.classList.add(darkModeCSSClass);
+                localStorage.setItem(themePersistenceKey, themePersistenceValueDark);
+                break;
+            }
+            default: {
+                darkModeMedia.addEventListener('change', darkModeMediaListener);
+                if (darkModeMedia.matches) {
+                    document.body.classList.add(darkModeCSSClass);
+                } else {
+                    document.body.classList.remove(darkModeCSSClass);
+                }
+                localStorage.removeItem(themePersistenceKey);
+                break;
+            }
         }
     });
-});
+}
