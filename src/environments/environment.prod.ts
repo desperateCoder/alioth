16940383de601d9bf4e02700e82f45b9c0ccfead
Stefan Niedermann <info@niedermann.it>
2021-12-23 12:56:50 +0100

    Provide prod environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  criteriaForInclusionURL: 'https://github.com/desperateCoder/alioth/blob/master/CONTRIBUTING.md#criteria-for-inclusion',
+  note: 'Please note that hentaiOS is not compatible with any of the listed kernels.',
+  maintainer: {
+    name: '@krakeen06',
+    url: 'https://t.me/PocoF3GlobalOfficial'
+  }
 };
