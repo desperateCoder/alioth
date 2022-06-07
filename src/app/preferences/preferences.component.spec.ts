593b26cbc2a8c5cf6e292fbf19699e6a871d2b2f
Stefan Niedermann <info@niedermann.it>
2022-06-07 09:14:50 +0200

    Update dependencies

--- src/app/preferences/preferences.component.spec.ts
+++ src/app/preferences/preferences.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { PreferencesComponent } from './preferences.component';
@@ -10,7 +16,13 @@ describe('PreferencesComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        TranslateModule.forRoot()
+        TranslateModule.forRoot(),
+        NoopAnimationsModule,
+        MatIconModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatFormFieldModule,
+        MatSelectModule
       ],
       declarations: [ PreferencesComponent ]
     })
